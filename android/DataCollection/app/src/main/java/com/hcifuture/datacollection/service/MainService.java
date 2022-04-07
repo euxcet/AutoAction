0a285326aabb46beb1564e932b332fe216e2a0c1
Atomie CHEN <atomic_cwh@163.com>
2022-04-07 17:42:50 +0800

    Update MainService listening broadcast events and content changes

--- android/DataCollection/app/src/main/java/com/hcifuture/datacollection/service/MainService.java
+++ android/DataCollection/app/src/main/java/com/hcifuture/datacollection/service/MainService.java
@@ -1,6 +1,7 @@
 package com.hcifuture.datacollection.service;
 
 import android.accessibilityservice.AccessibilityService;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -42,6 +43,7 @@ import com.lzy.okgo.model.Response;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -52,8 +54,97 @@ public class MainService extends AccessibilityService implements ContextListener
     private Handler mHandler;
 
     private CustomBroadcastReceiver mBroadcastReceiver;
+    private CustomContentObserver mContentObserver;
 
     private LoaderManager loaderManager;
+
+    // listening
+    private final Uri[] listenedURIs = {
+            Settings.System.CONTENT_URI,
+            Settings.Global.CONTENT_URI,
+    };
+    private final String [] listenedActions = {
+            Intent.ACTION_AIRPLANE_MODE_CHANGED,
+            Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED,
+            Intent.ACTION_BATTERY_LOW,
+            Intent.ACTION_BATTERY_OKAY,
+            Intent.ACTION_BOOT_COMPLETED,
+            Intent.ACTION_CONFIGURATION_CHANGED,
+            Intent.ACTION_DOCK_EVENT,
+            Intent.ACTION_DREAMING_STARTED,
+            Intent.ACTION_DREAMING_STOPPED,
+            Intent.ACTION_EXTERNAL_APPLICATIONS_AVAILABLE,
+            Intent.ACTION_EXTERNAL_APPLICATIONS_UNAVAILABLE,
+            Intent.ACTION_HEADSET_PLUG,
+            Intent.ACTION_INPUT_METHOD_CHANGED,
+            Intent.ACTION_LOCALE_CHANGED,
+            Intent.ACTION_LOCKED_BOOT_COMPLETED,
+            Intent.ACTION_MEDIA_BAD_REMOVAL,
+            Intent.ACTION_MEDIA_BUTTON,
+            Intent.ACTION_MEDIA_CHECKING,
+            Intent.ACTION_MEDIA_EJECT,
+            Intent.ACTION_MEDIA_MOUNTED,
+            Intent.ACTION_MEDIA_NOFS,
+            Intent.ACTION_MEDIA_REMOVED,
+            Intent.ACTION_MEDIA_SCANNER_FINISHED,
+            Intent.ACTION_MEDIA_SCANNER_STARTED,
+            Intent.ACTION_MEDIA_SHARED,
+            Intent.ACTION_MEDIA_UNMOUNTABLE,
+            Intent.ACTION_MEDIA_UNMOUNTED,
+            Intent.ACTION_MY_PACKAGE_REPLACED,
+            Intent.ACTION_PACKAGES_SUSPENDED,
+            Intent.ACTION_PACKAGES_UNSUSPENDED,
+            Intent.ACTION_PACKAGE_ADDED,
+            Intent.ACTION_PACKAGE_CHANGED,
+            Intent.ACTION_PACKAGE_DATA_CLEARED,
+            Intent.ACTION_PACKAGE_FIRST_LAUNCH,
+            Intent.ACTION_PACKAGE_FULLY_REMOVED,
+            Intent.ACTION_PACKAGE_NEEDS_VERIFICATION,
+            Intent.ACTION_PACKAGE_REMOVED,
+            Intent.ACTION_PACKAGE_REPLACED,
+            Intent.ACTION_PACKAGE_RESTARTED,
+            Intent.ACTION_PACKAGE_VERIFIED,
+            Intent.ACTION_POWER_CONNECTED,
+            Intent.ACTION_POWER_DISCONNECTED,
+            Intent.ACTION_PROVIDER_CHANGED,
+            Intent.ACTION_REBOOT,
+            Intent.ACTION_SCREEN_OFF,
+            Intent.ACTION_SCREEN_ON,
+            Intent.ACTION_SHUTDOWN,
+            Intent.ACTION_TIMEZONE_CHANGED,
+            Intent.ACTION_TIME_CHANGED,
+            Intent.ACTION_UID_REMOVED,
+            Intent.ACTION_USER_BACKGROUND,
+            Intent.ACTION_USER_FOREGROUND,
+            Intent.ACTION_USER_PRESENT,
+            Intent.ACTION_USER_UNLOCKED,
+            // Bluetooth related
+            BluetoothDevice.ACTION_ACL_CONNECTED,
+            BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED,
+            BluetoothDevice.ACTION_ACL_DISCONNECTED,
+    };
+
+    private static final HashMap<String, Integer> volume = new HashMap<>();
+    static {
+        // speaker
+        volume.put("volume_music_speaker", 0);
+        volume.put("volume_ring_speaker", 0);
+        volume.put("volume_alarm_speaker", 0);
+        volume.put("volume_voice_speaker", 0);
+        volume.put("volume_tts_speaker", 0);
+        // headset
+        volume.put("volume_music_headset", 0);
+        volume.put("volume_voice_headset", 0);
+        volume.put("volume_tts_headset", 0);
+        // headphone
+        volume.put("volume_music_headphone", 0);
+        volume.put("volume_voice_headphone", 0);
+        volume.put("volume_tts_headphone", 0);
+        // Bluetooth A2DP
+        volume.put("volume_music_bt_a2dp", 0);
+        volume.put("volume_voice_bt_a2dp", 0);
+        volume.put("volume_tts_bt_a2dp", 0);
+    }
 
     public MainService() {
     }
@@ -79,28 +170,37 @@ public class MainService extends AccessibilityService implements ContextListener
     protected void onServiceConnected() {
         super.onServiceConnected();
         mContext = this;
-        mBroadcastReceiver = new CustomBroadcastReceiver();
-        final IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-        filter.addAction(Intent.ACTION_SCREEN_ON);
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
-        filter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
-        filter.addAction(Intent.ACTION_MEDIA_BUTTON);
-        filter.addAction(Intent.ACTION_SET_WALLPAPER);
-        registerReceiver(mBroadcastReceiver, filter);
-
         this.mHandler = new Handler(Looper.getMainLooper());
         this.loaderManager = new LoaderManager(this, this, this);
         loaderManager.start();
+
+        // register broadcast receiver
+        mBroadcastReceiver = new CustomBroadcastReceiver();
+        IntentFilter filter = new IntentFilter();
+        for (String action : listenedActions) {
+            filter.addAction(action);
+        }
+        registerReceiver(mBroadcastReceiver, filter);
+
+        // register content observer
+        mContentObserver = new CustomContentObserver(new Handler());
+        for (Uri uri : listenedURIs) {
+            getContentResolver().registerContentObserver(uri, true, mContentObserver);
+        }
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
+    public boolean onUnbind(Intent intent) {
+        // unregister broadcast receiver
+        unregisterReceiver(mBroadcastReceiver);
+        // unregister content observer
+        getContentResolver().unregisterContentObserver(mContentObserver);
+
         if (loaderManager != null) {
             loaderManager.stop();
         }
+
+        return super.onUnbind(intent);
     }
 
     @Override
