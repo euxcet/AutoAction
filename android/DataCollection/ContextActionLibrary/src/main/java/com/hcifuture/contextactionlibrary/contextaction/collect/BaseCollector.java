f33bcdabb4f55283de4963f4f5b95fbb81bdea7e
Atomie CHEN <atomic_cwh@163.com>
2022-04-14 20:00:35 +0800

    Add getUserID() in BaseCollector, TO BE IMPLEMENTED in the future

--- android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/contextaction/collect/BaseCollector.java
+++ android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/contextaction/collect/BaseCollector.java
@@ -61,4 +61,9 @@ public abstract class BaseCollector {
         }
         return null;
     }
+
+    public static String getUserID() {
+        // TODO: implement in the future
+        return getMacMoreThanM();
+    }
 }
