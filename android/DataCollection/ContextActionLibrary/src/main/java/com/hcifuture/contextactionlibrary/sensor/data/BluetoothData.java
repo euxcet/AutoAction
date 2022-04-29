3e1fc0eefa6c2890d149be47589e8192c081aed4
Atomie CHEN <atomic_cwh@163.com>
2022-04-29 21:11:26 +0800

    Fix: BluetoothData synchronize methods

--- android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/sensor/data/BluetoothData.java
+++ android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/sensor/data/BluetoothData.java
@@ -5,49 +5,48 @@ import android.os.Build;
 import androidx.annotation.RequiresApi;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BluetoothData extends Data {
-    private List<SingleBluetoothData> data;
+    private final List<SingleBluetoothData> data;
 
     public BluetoothData() {
-        data = new ArrayList<>();
-    }
-
-    public void setData(List<SingleBluetoothData> data) {
-        this.data = data;
-    }
-
-    public List<SingleBluetoothData> getData() {
-        return data;
+        data = Collections.synchronizedList(new ArrayList<>());
     }
 
     public void clear() {
-        this.data.clear();
+        synchronized (data) {
+            data.clear();
+        }
     }
 
     public void insert(SingleBluetoothData single) {
-        for (int i = 0; i < data.size(); i++) {
-            SingleBluetoothData old = data.get(i);
-            if (old.getAddress().equals(single.getAddress()) && old.getLinked() == single.getLinked()) {
-                if (single.getScanResult().equals("") && !old.getScanResult().equals("")) {
-                    single.setScanResult(old.getScanResult());
+        synchronized (data) {
+            for (int i = 0; i < data.size(); i++) {
+                SingleBluetoothData old = data.get(i);
+                if (old.getAddress().equals(single.getAddress()) && old.getLinked() == single.getLinked()) {
+                    if (single.getScanResult().equals("") && !old.getScanResult().equals("")) {
+                        single.setScanResult(old.getScanResult());
+                    }
+                    if (single.getIntentExtra().equals("") && !old.getIntentExtra().equals("")) {
+                        single.setIntentExtra(old.getIntentExtra());
+                    }
+                    data.set(i, single);
+                    return;
                 }
-                if (single.getIntentExtra().equals("") && !old.getIntentExtra().equals("")) {
-                    single.setIntentExtra(old.getIntentExtra());
-                }
-                data.set(i, single);
-                return;
             }
+            data.add(single);
         }
-        data.add(single);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public BluetoothData deepClone() {
-        BluetoothData data = new BluetoothData();
-        getData().forEach(data::insert);
-        return data;
+        synchronized (data) {
+            BluetoothData bluetoothData = new BluetoothData();
+            data.forEach(bluetoothData::insert);
+            return bluetoothData;
+        }
     }
 
     @Override
