279962837227681561e859511ee0ee597474c567
Atomie CHEN <atomic_cwh@163.com>
2022-05-07 21:15:06 +0800

    WifiCollector records wifi state

--- android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/sensor/data/WifiData.java
+++ android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/sensor/data/WifiData.java
@@ -9,37 +9,47 @@ import java.util.Collections;
 import java.util.List;
 
 public class WifiData extends Data {
-    private final List<SingleWifiData> data;
+    private final List<SingleWifiData> aps;
+    private int state;
 
     public WifiData() {
-        data = Collections.synchronizedList(new ArrayList<>());
+        aps = Collections.synchronizedList(new ArrayList<>());
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
     }
 
     public void clear() {
-        synchronized (data) {
-            data.clear();
+        synchronized (aps) {
+            aps.clear();
         }
     }
 
     public void insert(SingleWifiData single) {
-        synchronized (data) {
-            for (int i = 0; i < data.size(); i++) {
-                if (data.get(i).getBssid().equals(single.getBssid()) && data.get(i).getConnected() == single.getConnected()) {
-                    data.set(i, single);
+        synchronized (aps) {
+            for (int i = 0; i < aps.size(); i++) {
+                if (aps.get(i).getBssid().equals(single.getBssid()) && aps.get(i).getConnected() == single.getConnected()) {
+                    aps.set(i, single);
                     return;
                 }
             }
-            data.add(single);
+            aps.add(single);
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public WifiData deepClone() {
-        synchronized (data) {
-            WifiData wifiData = new WifiData();
-            data.forEach(wifiData::insert);
-            return wifiData;
+        WifiData wifiData = new WifiData();
+        synchronized (aps) {
+            aps.forEach(wifiData::insert);
         }
+        wifiData.setState(getState());
+        return wifiData;
     }
 
     @Override
