156e6fe8ab515c033b0476d100f3dcc1ec0ef408
Atomie CHEN <atomic_cwh@163.com>
2022-05-20 20:38:42 +0800

    CollectorException can be a exception wrapper

--- android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/sensor/collector/CollectorException.java
+++ android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/sensor/collector/CollectorException.java
@@ -1,10 +1,18 @@
 package com.hcifuture.contextactionlibrary.sensor.collector;
 
+import androidx.annotation.NonNull;
+
 public class CollectorException extends Exception {
 
     private final int code;
+
     public CollectorException(int code, String msg) {
         super(msg);
+        this.code = code;
+    }
+
+    public CollectorException(int code, Throwable cause) {
+        super(cause);
         this.code = code;
     }
 
@@ -12,6 +20,7 @@ public class CollectorException extends Exception {
         return code;
     }
 
+    @NonNull
     @Override
     public String toString() {
         return "CollectorException(" + code + "): " + getLocalizedMessage();
